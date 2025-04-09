d248ce20949a0f089638c6cbeaf0788011030a4e
kgulbarg <kgulbarg@asu.edu>
2025-04-09 13:50:01 -0700

    Set up jest dependencies, test script and test suite

---
+++ app/frontend/src/test/CodeComment.test.js
@@ -0,0 +1,9 @@
+jest.mock("axios");
+
+describe("CodeComment Component", () => {
+
+  test("Trivial test for test suite", () => {
+    expect(true).toBe(true);
+  });
+
+});
---
+++ frontend/jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
+  testPathIgnorePatterns: ['/node_modules/', '/build/'],
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!(axios|react-router-dom|@testing-library|@mui|chart.js)/)',
+  ],
+  moduleNameMapper: {
+    '^react-router-dom$': '<rootDir>/node_modules/react-router-dom',
+  },
+};
