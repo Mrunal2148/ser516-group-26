eb587d5d05ded28e97687e2f519c62287dfdf7bc
Mrunal Kapure <mkapure@asu.edu>
2025-04-27 20:08:02 -0700

    Update test_code_comment_coverage.py

--- app/backend/python-backend/code-comment/tests/test_code_comment_coverage.py
+++ app/backend/python-backend/code-comment/tests/test_code_comment_coverage.py
@@ -4,6 +4,10 @@ import shutil
 from flask import json
 from code_comment_app import app, save_to_json, clone_repo, get_code_files, process_file, calculate_comment_coverage
 
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../")))
+from utilities.fetch_repo import fetch_repo, get_owner_repo
+from utilities.response_wrapper import wrap_with_timestamp
 
 class TestApp(unittest.TestCase):
 
